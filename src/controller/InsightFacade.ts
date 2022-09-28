34c508f238b26528d959c27a45c878f66cd4940b
chsyan <charles5850@gmail.com>
2022-09-27 19:14:26 -0700

    JSZip read file and use InsightDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,23 +1,16 @@
-import {
-	IInsightFacade,
-	InsightDataset,
-	InsightDatasetKind,
-	InsightError,
-	InsightResult,
-	NotFoundError,
-} from "./IInsightFacade";
+import JSZip, {JSZipObject} from "jszip";
+import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, InsightResult} from "./IInsightFacade";
 
-/**
+/*
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
- *
  */
 export default class InsightFacade implements IInsightFacade {
-	private ids: string[];
+	private insightDatasets: InsightDataset[];
 
 	constructor() {
 		console.log("InsightFacadeImpl::init()");
-		this.ids = [];
+		this.insightDatasets = [];
 	}
 
 	private validateId(id: string): void {
@@ -46,15 +39,37 @@ export default class InsightFacade implements IInsightFacade {
 		this.validateId(id);
 
 		// Check for duplicate ids
-		if (this.ids.includes(id)) {
-			throw new InsightError("id already exists");
+		const ids = [];
+		for (const dataset in this.insightDatasets) {
+			const storedId = this.insightDatasets[dataset].id;
+			ids.push(storedId);
+			if (storedId === id) {
+				throw new InsightError("id already exists");
+			}
 		}
 
+		// Content comes in as string decoded from b64, but JSZip requires it to be encoded
+		const contentEncoded = Buffer.from(content, "base64");
+		const zipContent = await JSZip.loadAsync(contentEncoded);
+		zipContent.forEach(async (_path: string, file: JSZipObject) => {
+			if (!file.dir) {
+				const fileContent = await file.async("string");
+				const obj = JSON.parse(fileContent);
+				// Entries are in a result array
 		// TODO: Process content into a data structure
+			}
+		});
+
 		// TODO: Write dataset to disk
 
-		this.ids.push(id);
-		return this.ids;
+		const newDataset: InsightDataset = {
+			id: id,
+			kind: kind,
+			numRows: 0, // TODO: Get the number of rows
+		};
+		this.insightDatasets.push(newDataset);
+		ids.push(id);
+		return ids;
 	}
 
 	public removeDataset(id: string): Promise<string> {
