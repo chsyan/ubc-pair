ed372ed32ddacf6566897284c8e986e09a40dd87
Brandon Yih <brandon8@telus.net>
2022-10-03 02:01:20 -0700

    Implemented performQuery EBNF-OPTIONS Validation

--- src/controller/QueryValidator.ts
+++ src/controller/QueryValidator.ts
@@ -1,16 +1,16 @@
 import {InsightError} from "./IInsightFacade";
 
-const isExistingObject = (maybeObj: any, maybeObjName: string): boolean => {
+const isExistingObject = (maybeObj: any, objName: string): boolean => {
 	if (maybeObj === undefined) {
 		return false;
 	}
 	if (typeof maybeObj !== "object") {
-		throw new InsightError(maybeObjName + " must be an object");
+		throw new InsightError(objName + " must be an object");
 	}
 	return true;
 };
 
-const numberKeyChecker = (obj: object, objName: string, expectedKeys: number): void => {
+const checkKeysLength = (obj: object, objName: string, expectedKeys: number): void => {
 	let keyOrKeys = " keys";
 	if (expectedKeys === 1) {
 		keyOrKeys = " key";
@@ -20,32 +20,50 @@ const numberKeyChecker = (obj: object, objName: string, expectedKeys: number): v
 	}
 };
 
+const checkNonEmptyArray = (arr: any, key: string): void => {
+	if (!Array.isArray(arr) || arr.length === 0) {
+		throw new InsightError(key + " must be a non-empty array");
+	}
+};
+
+const isValidKey = (key: any, type?: "mkey" | "skey"): boolean => {
+	if (typeof key !== "string") {
+		return false;
+	}
+	const isValidMKey = key.match(/^[^_]+_(avg|pass|fail|audit|year)$/g) !== null;
+	const isValidSKey = key.match(/^[^_]+_(dept|id|instructor|title|uuid)$/g) !== null;
+	if (type === "mkey") {
+		return isValidMKey;
+	} else if (type === "skey") {
+		return isValidSKey;
+	}
+	return isValidSKey || isValidMKey;
+};
+
 const validateFilter = (filter: any, name: string): void => {
-	numberKeyChecker(filter, name, 1);
+	checkKeysLength(filter, name, 1);
 	if (isExistingObject(filter.LT, "LT") ||
 			isExistingObject(filter.EQ, "EQ") ||
 			isExistingObject(filter.GT, "GT")) {
 		const mComparator = Object.keys(filter)[0];
-		numberKeyChecker(filter[mComparator], mComparator, 1);
+		checkKeysLength(filter[mComparator], mComparator, 1);
 		const mKey = Object.keys(filter[mComparator])[0];
-		if (!mKey.match(/^[^_]+_(avg|pass|fail|audit|year)$/g) || typeof filter[mComparator][mKey] !== "number") {
+		if (!isValidKey(mKey, "mkey") || typeof filter[mComparator][mKey] !== "number") {
 			throw new InsightError("invalid mkey or value used in " + mComparator);
 		}
 	} else if (isExistingObject(filter.IS, "IS")) {
-		numberKeyChecker(filter.IS, "IS", 1);
+		checkKeysLength(filter.IS, "IS", 1);
 		const sKey = Object.keys(filter.IS)[0];
 		let invalidIsString = typeof filter.IS[sKey] !== "string" ||
 				!filter.IS[sKey].match(/^\*?[^*]*\*?$/g);
-		if (!sKey.match(/^[^_]+_(dept|id|instructor|title|uuid)$/g) || invalidIsString) {
+		if (!isValidKey(sKey, "skey") || invalidIsString) {
 			throw new InsightError("invalid skey or value used");
 		}
 	} else if (isExistingObject(filter.NOT, "NOT")) {
 		validateFilter(filter.NOT, "NOT");
-	} else if (isExistingObject(filter.OR, "OR") || isExistingObject(filter.AND, "AND")) {
+	} else if (filter.OR !== undefined || filter.AND !== undefined) {
 		const logic = Object.keys(filter)[0];
-		if(!Array.isArray(filter[logic]) || filter[logic].length === 0) {
-			throw new InsightError(logic + " must be a non-empty array");
-		}
+		checkNonEmptyArray(filter[logic], logic);
 		for (const logicFilter of filter[logic]) {
 			validateFilter(logicFilter, "Filters in " + logic);
 		}
@@ -54,26 +72,53 @@ const validateFilter = (filter: any, name: string): void => {
 	}
 };
 
+const validateOptions = (opt: any): void => {
+	const optKeysLength = Object.keys(opt).length;
+
+	// Validate COLUMNS
+	if (optKeysLength === 0 || opt.COLUMNS === undefined) {
+		throw new InsightError("OPTIONS missing COLUMNS");
+	} else if (optKeysLength <= 2) {
+		checkNonEmptyArray(opt.COLUMNS, "COLUMNS");
+		for (const column of opt.COLUMNS) {
+			if (!isValidKey(column)) {
+				throw new InsightError("Invalid key " + column + " in COLUMNS");
+			}
+		}
+	} else {
+		throw new InsightError("OPTIONS has too many keys");
+	}
+
+	// Validate Possible ORDER
+	if (optKeysLength === 2 && opt.ORDER === undefined) {
+		throw new InsightError("Invalid key in OPTIONS");
+	} else if (optKeysLength === 2 && !isValidKey(opt.ORDER)) {
+		throw new InsightError("Invalid key " + opt.ORDER + " in COLUMNS");
+	}
+};
+
 const validateQuery = (query: any): void => {
-		// Validate Syntax
+	// Validate Syntax
 	if (Object.keys(query).length !== 2) {
 		throw new InsightError("query should only have 2 keys");
 	}
-	numberKeyChecker(query, "Query", 2);
+	checkKeysLength(query, "Query", 2);
 	if (!isExistingObject(query.WHERE, "WHERE")) {
 		throw new InsightError("query missing WHERE");
 	}
 	if (!isExistingObject(query.OPTIONS, "OPTIONS")) {
 		throw new InsightError("query missing OPTIONS");
 	}
-		// Validate Syntax - WHERE Block
+
+	// Validate Syntax - WHERE Block
 	if (Object.keys(query.WHERE).length !== 0) {
 		validateFilter(query.WHERE, "WHERE");
 	}
 
-		// TODO: Validate Syntax - OPTIONS Block
+	// Validate Syntax - OPTIONS Block
+	validateOptions(query.OPTIONS);
 
-		// TODO: Validate Semantic
+	// TODO: Validate Semantic
 };
 
 export {validateQuery};
